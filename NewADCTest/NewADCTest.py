d481dd5175a08e891bc4c4afb6c7688f3b38dcaf
Samuel Lund Jessen <au646967@uni.au.dk>
2022-05-22 20:51:25 +0200

    new

--- NewADCTest/NewADCTest.py
+++ NewADCTest/NewADCTest.py
@@ -1,19 +1,22 @@
+import time
 import board
-import digitalio
 import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
+# Create the I2C bus
 i2c = busio.I2C(board.SCL, board.SDA)
 
-adsforceGND = ADS.ADS1015(address=48,i2c=i2c,gain=2/3)
-adstemplightVCC = ADS.ADS1015(address=49,i2c=i2c,gain=2/3)
+# Create the ADC object using the I2C bus
+ads = ADS.ADS1015(i2c)
 
-chanforce = AnalogIn(adsforceGND, ADS.P0)
-chanlight = AnalogIn(adstemplightVCC, ADS.P0)
+# Create single-ended input on channel 0
+chan = AnalogIn(ads, ADS.P0)
 
+# Create differential input between channel 0 and 1
+# chan = AnalogIn(ads, ADS.P0, ADS.P1)
+
+print("{:>5}\t{:>5}".format("raw", "v"))
 
 while True:
-    print("Force adc (GND):" + str(chanforce.value), str(chan.voltage))
-    print("Light adc (VCC):" + str(chanlight.value), str(chanlight.voltage))
-
-
+    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+    time.sleep(0.5)
