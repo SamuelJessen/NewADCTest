9c5c49396c27230088197770385aeb3304772bcf
Samuel Lund Jessen <au646967@uni.au.dk>
2022-05-22 20:46:27 +0200

    new

--- NewADCTest/NewADCTest.py
+++ NewADCTest/NewADCTest.py
@@ -5,8 +5,8 @@ import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 i2c = busio.I2C(board.SCL, board.SDA)
 
-adsforceGND = ADS.ADS1015(address=0x48,i2c=i2c,gain=2/3)
-adstemplightVCC = ADS.ADS1015(address=0x49,i2c=i2c,gain=2/3)
+adsforceGND = ADS.ADS1015(address=48,i2c=i2c,gain=2/3)
+adstemplightVCC = ADS.ADS1015(address=49,i2c=i2c,gain=2/3)
 
 chanforce = AnalogIn(adsforceGND, ADS.P0)
 chanlight = AnalogIn(adstemplightVCC, ADS.P0)
