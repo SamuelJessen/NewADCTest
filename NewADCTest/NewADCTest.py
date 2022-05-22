c5da53ff90f9c80e3ab7a3a63f6e3237dde120d5
Samuel Lund Jessen <au646967@uni.au.dk>
2022-05-22 22:17:43 +0200

    new

--- NewADCTest/NewADCTest.py
+++ NewADCTest/NewADCTest.py
@@ -16,8 +16,8 @@ chan = AnalogIn(ads, ADS.P0)
 # Create differential input between channel 0 and 1
 # chan = AnalogIn(ads, ADS.P0, ADS.P1)
 
-print("{:>5}\t{:>5}".format("raw", "v"))
+#print("{:>5}\t{:>5}".format("raw", "v"))
 
 while True:
-    print("{:>5}\t{:>5.3f}".format(chan.value, chan.voltage))
+    print(chan.value, chan.voltage)
     time.sleep(0.5)
